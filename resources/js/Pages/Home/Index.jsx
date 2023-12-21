68c55f76f118a383b4feac5e21a0537cedb3e989
mikedubcurry <michaelcurry95@gmail.com>
2023-12-20 19:39:15 -0500

    mocking out some screens

--- resources/js/Pages/Home/Index.jsx
+++ resources/js/Pages/Home/Index.jsx
@@ -1,18 +1,71 @@
 import React from 'react'
 import { Link } from '@inertiajs/react'
 
-export default function Home({ gyms }) {
+export default function Home({ gyms, disciplines, user }) {
+    // create array of session/recovery days spanning the last 30 days
+    console.log(user)
     return (
-        <div className='w-10/12 mx-auto'>
-            <h1 className='text-6xl font-bold'>Home</h1>
-            <div className='grid grid-cols-3 gap-4'>
-                {gyms.map(gym => (
-                    <div className='bg-gray-200 rounded-lg shadow-lg p-4' key={gym.slug}>
-                        <h2 className='text-2xl font-bold'>{gym.name}</h2>
-                        <Link href={`/gyms/${gym.slug}`} className='text-blue-500'>View Gym</Link>
+        <>
+            <header className='flex justify-between items-center px-8 py-4 border-b'>
+                <h1 className='font-bold text-3xl'>Martial Arts</h1>
+                <div className='flex items-center'>
+                    <div className='rounded-full h-10 w-10 bg-gray-400'></div>
+                </div>
+            </header>
+            <main className='w-10/12 mx-auto py-4'>
+                <div className=''>
+                    <Link href='/session/create' className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Record New Session</Link>
+                </div>
+
+                <div className='mt-4'>
+                    <h2 className='text-2xl font-bold'>Past Sessions</h2>
+                    <div className=''>
+                        <ul className=''>
+                            {user.gym_sessions.map((session) => (
+                                <li className='flex justify-between items-center border-b py-2 hover:bg-gray-50' key={session.id}>
+                                    <div className='flex items-center gap-4'>
+                                        <p>{session.date}</p>
+                                        <p>{session.discipline.discipline}</p>
+                                        <p>{session.start_time}</p>
+                                        <p>{session.end_time}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                ))}
-            </div>
-        </div>
+                </div>
+                <div className='mt-4'>
+                    <h2 className='text-2xl font-bold'>Recovery</h2>
+                    <div className=''>
+                        <ul className=''>
+                            {user.recoveries.map((recovery) => (
+                                <li className='flex justify-between items-center border-b py-2 hover:bg-gray-50' key={recovery.id}>
+                                    <div className='flex items-center gap-4'>
+                                        <p>{recovery.date}</p>
+                                        <p>{recovery.type}</p>
+                                        <p>{recovery.notes}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className='mt-4'>
+                    <h2 className='text-2xl font-bold'>Explore Martial Arts</h2>
+                    <div className=''>
+                        <ul className=''>
+                            {disciplines.map((discipline) => (
+                                <li className='flex justify-between items-center border-b py-2 hover:bg-gray-50' key={discipline.id}>
+                                    <div className='w-full flex justify-between items-center gap-4'>
+                                        <p>{discipline.discipline}</p>
+                                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Find a Gym</button>
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </main>
+        </>
     )
 }
